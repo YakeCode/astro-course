b18a9d8140ca5e170923b35515253a0dfbb35789
YakeCode <y.gaviriaescobar@gmail.com>
2025-02-12 22:17:47 -0500

    comenzando a entender las server islands

--- dev-books/astro.config.mjs
+++ dev-books/astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig, envField } from "astro/config";
 
 export default defineConfig({
+  output: "server",
   integrations: [tailwind()],
   env: {
     schema: {
